cf661a2379e23961fff58ecae8b89e8e9bf8ed0b
motumag <motumagishu27@gmail.com>
2023-05-31 14:43:51 +0300

    added new ekyc

--- routers/eKyc.router.js
+++ routers/eKyc.router.js
@@ -1,27 +1,9 @@
 const express = require("express");
 const multer = require("multer");
-const eKycRouter = express.Router();
+const router = express.Router();
 const path = require("path");
 const { createNewEkyc } = require("../controllers/eKyc.controller"); // Import the controller function
-// Set up Multer and other configurations
-const storage = multer.diskStorage({
-  destination: "./uploads", // Specify the directory to save the uploaded files
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    const fileName = file.fieldname + "-" + uniqueSuffix + ext;
-    cb(null, fileName);
-  },
-});
-const upload = multer({ storage });
+
 // API endpoint to handle image uploads
-eKycRouter.post(
-  "/create",
-  upload.fields([
-    { name: "agreement_doc", maxCount: 1 },
-    { name: "business_license", maxCount: 1 },
-    { name: "valid_identification", maxCount: 1 },
-  ]),
-  createNewEkyc
-);
-module.exports = eKycRouter;
+router.post("/create", createNewEkyc);
+module.exports = router;
--- routers/item.router.js
+++ routers/item.router.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const multer =require("multer")
+const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const {grantAccess} = require('../middlewares/userVerification');
+const { grantAccess } = require("../middlewares/userVerification");
 
-    // Set up Multer storage
-    const storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Set the destination directory for uploaded images
-        },
-        filename: (req, file, cb) => {
-        cb(null, file.originalname); // Use the original filename
-        },
-    });
-    // Create the Multer upload instance
-    const upload = multer({ storage: storage });
-    
+// Set up Multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Set the destination directory for uploaded images
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Use the original filename
+  },
+});
+// Create the Multer upload instance
+const upload = multer({ storage: storage });
 
 // Import your controllers
 const {
-    createNewItem,
-    getAllItems,
-    getItemsById,
-    assignItemsToSales,
-    assignItemsToSalesApprove,
-    configureLoanForitem
-} = require('../controllers/item.controller');
+  createNewItem,
+  getAllItems,
+  getItemsById,
+  assignItemsToSales,
+  assignItemsToSalesApprove,
+  configureLoanForitem,
+} = require("../controllers/item.controller");
 // User routes
 
-router.post('/create', grantAccess(['merchant']), upload.single('picture'), createNewItem);
-router.get('/getAll', grantAccess(['merchant','sales']),  getAllItems);
-router.get('/getById',grantAccess(['merchant','sales']),  getItemsById);
-router.post('/assigntoSales',grantAccess(['merchant']),  assignItemsToSales);
-router.post('/acceptItem',grantAccess(['sales']),  assignItemsToSalesApprove);
-router.post('/configureLoanForItem',grantAccess(['merchant']),  configureLoanForitem);
+router.post(
+  "/create",
+  grantAccess(["merchant"]),
+  upload.single("picture"),
+  createNewItem
+);
+router.get("/getAll", grantAccess(["merchant", "sales"]), getAllItems);
+router.get("/getById", grantAccess(["merchant", "sales"]), getItemsById);
+router.post("/assigntoSales", grantAccess(["merchant"]), assignItemsToSales);
+router.post("/acceptItem", grantAccess(["sales"]), assignItemsToSalesApprove);
+router.post(
+  "/configureLoanForItem",
+  grantAccess(["merchant"]),
+  configureLoanForitem
+);
 
 module.exports = router;
