f4acb16ddc1bccb9864918fcd5fb67ff2b67684b
Amir Edris <amaedris1@gmail.com>
2023-05-31 11:54:45 +0300

    track Untracked file

---
+++ models/LoanConfig.models.js
@@ -0,0 +1,18 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize=require("../configs/db")
+const LoanConf = sequelize.define("loanconf", {
+    loan_conf_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    interest_rate: {
+      type: DataTypes.DECIMAL(5,2),
+      allowNull: true,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+    allowNull:false
+    },
+  });
+module.exports=LoanConf;
