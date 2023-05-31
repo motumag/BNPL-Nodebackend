1c027ac2a97b059e86b02cf3b83008960f258500
motumag <motumagishu27@gmail.com>
2023-05-31 11:31:12 +0300

    ekyc added

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require("express")
-const cookieSession = require("cookie-session")
-const {json}=require("body-parser")
-const cors = require('cors');
-const db = require("./configs/db")
-const merchantManagementRouter=require("./usermanagement/router/merchant")
-const itemRouter=require("./routers/item.router")
-const loanRouter=require("./routers/loan.router")
-const salesRouter=require("./routers/sales.router")
-const app = express()
-app.use(json())
+const express = require("express");
+const cookieSession = require("cookie-session");
+const { json } = require("body-parser");
+const cors = require("cors");
+const db = require("./configs/db");
+const merchantManagementRouter = require("./usermanagement/router/merchant");
+const itemRouter = require("./routers/item.router");
+const eKycRouter = require("./routers/eKyc.router");
+const loanRouter = require("./routers/loan.router");
+const salesRouter = require("./routers/sales.router");
+const app = express();
+app.use(json());
 app.use(
-    cookieSession({
-        signed:false,
-        secure:true
-    })
-)
-app.use(cors("*"))
-app.use("/image",express.static("uploads"))
-app.use('/api/merchant',merchantManagementRouter)
-app.use('/api/items',itemRouter)
-app.use('/api/loan',loanRouter)
-app.use('/api/sales',salesRouter)
-const PORT=process.env.PORT
-app.listen(5000,()=>{
-    console.info(`Running On Port 5000`)
-})
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+app.use(cors("*"));
+app.use("/image", express.static("uploads"));
+app.use("/api/merchant", merchantManagementRouter);
+app.use("/api/items", itemRouter);
+app.use("/api/eky", eKycRouter);
+app.use("/api/loan", loanRouter);
+app.use("/api/sales", salesRouter);
+const PORT = process.env.PORT;
+app.listen(5000, () => {
+  console.info(`Running On Port 5000`);
+});
--- usermanagement/models/merchant.model.js
+++ usermanagement/models/merchant.model.js
@@ -1,42 +1,42 @@
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize=require("../../configs/db")
-const Sales = require("./sales.model")
-const Items = require("../../models/item.model")
-const LoanConf = require("../../models/LoanConfig.models")
+const sequelize = require("../../configs/db");
+const Sales = require("./sales.model");
+const Items = require("../../models/item.model");
+const LoanConf = require("../../models/LoanConfig.models");
 
 const Merchant = sequelize.define("merchants", {
-    merchant_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    email_address: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    emailStatus: {
-      type: DataTypes.STRING,
-      enum: ["Pending", "Active", "Inactive"],
-      defaultValue: "Pending",
-    },
-    phone_number: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: "merchant",
-      allowNull: false,
-    },
-  });
-  Merchant.hasMany(Sales, {foreignKey:"merchant_id"})
-  Sales.belongsTo(Merchant,{foreignKey:"merchant_id"})
-  Merchant.hasMany(Items, {foreignKey:"merchant_id"})
-  Items.belongsTo(Merchant,{foreignKey:"merchant_id"})
-  Merchant.hasMany(LoanConf, {foreignKey:"merchant_id"})
-  LoanConf.belongsTo(Merchant, {foreignKey:"merchant_id"})
-module.exports=Merchant;
+  merchant_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email_address: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  emailStatus: {
+    type: DataTypes.STRING,
+    enum: ["Pending", "Active", "Inactive"],
+    defaultValue: "Pending",
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "merchant",
+    allowNull: false,
+  },
+});
+Merchant.hasMany(Sales, { foreignKey: "merchant_id" });
+Sales.belongsTo(Merchant, { foreignKey: "merchant_id" });
+Merchant.hasMany(Items, { foreignKey: "merchant_id" });
+Items.belongsTo(Merchant, { foreignKey: "merchant_id" });
+Merchant.hasMany(LoanConf, { foreignKey: "merchant_id" });
+LoanConf.belongsTo(Merchant, { foreignKey: "merchant_id" });
+module.exports = Merchant;
